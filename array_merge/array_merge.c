b36d3d7a7f1af0c1d83103e592daefa51ce6e569
Joshua Engelkes <jbengelkes@gmail.com>
2022-10-06 10:32:54 -0500

    When merging array lists, put all of the values into one array list and sort it.

--- array_merge/array_merge.c
+++ array_merge/array_merge.c
@@ -2,6 +2,26 @@
 #include "../mergesort/mergesort.h"
 
 int* array_merge(int num_arrays, int* sizes, int** values) {
-  // This is obviously broken. It has the right type, though, eh?
+  
+  int index = 0;
+  int *result = (int*) calloc(sizeof(values)/4 + 1, sizeof(int));
+  int *temp = (int*) calloc(sizeof(values)/4, sizeof(int));
+
+  // Put all numbers from the values 2D-array into the temporary array list
+  for (int i = 0; i < num_arrays; i++) {
+    for (int j = 0; j < sizes[i]; j++) {
+      temp[index] = values[i][j];
+      index++;
+    }
+  }
+
+  // Sort the values in the temporary array list
+  mergesort(sizeof(values)/4, temp);
+
+
+
+
+ 	
+
   return sizes;
 }
